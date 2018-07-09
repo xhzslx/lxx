5d9056a959ecf48680a5172246bcf8914489e2f3
xhzslx <464776245@qq.com>
2018-07-09 16:02:29 +0800

    Revert "第一次"
    This reverts commit 6682af59a01ec642b7357244fed60b0a3c8b15ff.

--- MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/base/IPresenter.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpm.librarydemo.tools.base;
-
-public interface IPresenter {
-    void setSuccessData2Presenter(String data, int code);
-
-    void setErrorData2Presenter(int code);
-}
--- MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/base/IView.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpm.librarydemo.tools.base;
-
-public interface IView<T> {
-    void setSuccessData2View(T t, int code);
-
-    void setErrorData2View(int code);
-}
