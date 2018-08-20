d579ff2a1433ecd170980774eb9bf15764cdde24
xhzslx <464776245@qq.com>
2018-08-20 10:08:01 +0800

    第二次

--- MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/util/HttpUtil.java
+++ MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/util/HttpUtil.java
@@ -1,9 +1,16 @@
 package com.example.jpm.librarydemo.tools.util;
 
+import android.os.Environment;
+import android.support.annotation.NonNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -37,6 +44,7 @@ public class HttpUtil {
                             callback.onError(code);
                         }
                     }
+
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
                         if (callback != null) {
@@ -46,6 +54,87 @@ public class HttpUtil {
                 });
             }
         }.start();
+    }
+
+    public static void okHttpDownloadFile(final String url, final FCallBack fCallBack,
+                                          final String path) {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder().url(url).build();
+                okHttpClient.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        // 下载失败
+                        fCallBack.onError();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        InputStream is = null;
+                        byte[] buf = new byte[2048];
+                        int len = 0;
+                        FileOutputStream fos = null;
+                        // 储存下载文件的目录
+                        String savePath = isExistDir(path);
+                        try {
+                            is = response.body().byteStream();
+                            long total = response.body().contentLength();
+                            File file = new File(savePath, getNameFromUrl(url));
+                            fos = new FileOutputStream(file);
+                            long sum = 0;
+                            while ((len = is.read(buf)) != -1) {
+                                fos.write(buf, 0, len);
+                                sum += len;
+                                int progress = (int) (sum * 1.0f / total * 100);
+                                // 下载中
+                                fCallBack.onLoading(total, progress);
+                            }
+                            fos.flush();
+                            // 下载完成
+                            fCallBack.onsuccess();
+                        } catch (Exception e) {
+                            fCallBack.onError();
+                        } finally {
+                            try {
+                                if (is != null)
+                                    is.close();
+                            } catch (IOException e) {
+                            }
+                            try {
+                                if (fos != null)
+                                    fos.close();
+                            } catch (IOException e) {
+                            }
+                        }
+                    }
+                });
+
+            }
+        }.start();
+
+    }
+
+    /**
+     * @param saveDir
+     * @return
+     * @throws IOException 判断下载目录是否存在
+     */
+    private static String isExistDir(String saveDir) throws IOException {
+        // 下载位置
+        File downloadFile = new File(Environment.getExternalStorageDirectory(), saveDir);
+        if (!downloadFile.mkdirs()) {
+            downloadFile.createNewFile();
+        }
+        String savePath = downloadFile.getAbsolutePath();
+        return savePath;
+    }
+
+    @NonNull
+    private static String getNameFromUrl(String url) {
+        return url.substring(url.lastIndexOf("/") + 1);
     }
 
     public static void okHttpPostMethod(final String url, final String bytes, final XCallBack callback, final int code) {
@@ -97,6 +186,8 @@ public class HttpUtil {
 
         void onError();
 
-        void onLoading(long total, long current, boolean isDownloading);
+        void onLoading(long total, long current);
     }
+
+
 }
