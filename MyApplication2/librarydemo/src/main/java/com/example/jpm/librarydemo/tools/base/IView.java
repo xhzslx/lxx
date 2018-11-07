582b920e729e2b9464ac6cf77ce18fd3bfcf5899
xhzslx <464776245@qq.com>
2018-11-07 11:17:00 +0800

    2018.11.07第四次

--- MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/base/IView.java
+++ MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/base/IView.java
@@ -25,9 +25,8 @@ public interface IView<T>{
     /**
      * @descriptoin	请求数据错误
      * @author	dc
-     * @param throwable 异常类型
      * @date 2017/2/16 11:01
      */
-    void loadDataError(Throwable throwable,int code);
+    void loadDataError(int code);
 
 }
