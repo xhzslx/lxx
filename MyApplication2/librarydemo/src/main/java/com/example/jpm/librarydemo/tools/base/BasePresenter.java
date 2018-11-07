8c69274192f79886fcbee7fa814d105338283f26
xhzslx <464776245@qq.com>
2018-11-07 18:01:55 +0800

    2018-11-07提交

--- MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/base/BasePresenter.java
+++ MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/base/BasePresenter.java
@@ -1,68 +1,23 @@
 package com.example.jpm.librarydemo.tools.base;
 
 /**
- * presenter的基础类
- * Created by lx464 on 2018/3/23.
+ * Created by mayn on 2018/11/7.
  */
-public class BasePresenter<V extends IPlusView, T> implements IBaseRequestCallBack<T> {
 
-    public IPlusView iBaseView = null;  //基类视图
+public class BasePresenter<T> {
+    private T mview;
 
-    /**
-     * @param view 具体业务的视图接口对象
-     * @descriptoin 构造方法
-     * @author dc
-     * @date 2017/2/16 15:12
-     */
-    public BasePresenter(V view) {
-        this.iBaseView = view;
+    public BasePresenter() {
     }
 
-    /**
-     * @descriptoin 请求之前显示progress
-     * @author dc
-     * @date 2017/2/16 15:13
-     */
-    @Override
-    public void beforeRequest() {
-        iBaseView.showProgress();
-    }
-
-    /**
-     * @descriptoin 请求异常显示异常信息
-     * @author dc
-     * @date 2017/2/16 15:13
-     */
-    @Override
-    public void requestError(int code) {
-        iBaseView.disimissProgress(); //请求错误，提示错误信息之后隐藏progress
-    }
-
-    /**
-     * @descriptoin 请求完成之后隐藏progress
-     * @author dc
-     * @date 2017/2/16 15:14
-     */
-    @Override
-    public void requestComplete() {
-        iBaseView.disimissProgress();
-    }
-
-    /**
-     * @param callBack 回调的数据
-     * @descriptoin 请求成功获取成功之后的数据信息
-     * @author dc
-     * @date 2017/2/16 15:14
-     */
-    @Override
-    public void requestSuccess(T callBack, int code) {
-        iBaseView.setSuccessData2View(callBack, code);
-    }
-
-    //Presenter注销接口防止内存泄漏
     public void detach() {
-        if (iBaseView != null) {
-            iBaseView = null;
+        if(this.mview != null) {
+            this.mview = null;
         }
+
+    }
+
+    public void attach(T view) {
+        this.mview = view;
     }
 }
