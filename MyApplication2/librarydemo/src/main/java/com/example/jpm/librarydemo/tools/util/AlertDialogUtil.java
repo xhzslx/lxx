3ec60fe2bde379f6d731eacad7dda66c33a5605e
罗霄 <464776245@qq.com>
2019-03-25 14:29:24 +0800

    小修改

--- MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/util/AlertDialogUtil.java
+++ MyApplication2/librarydemo/src/main/java/com/example/jpm/librarydemo/tools/util/AlertDialogUtil.java
@@ -123,6 +123,7 @@ public class AlertDialogUtil {
         alertDialog.setTitle(title);
         alertDialog.setMessage(message);
         alertDialog.show();
+        alertDialog.setCanceledOnTouchOutside(false);
         return builder;
     }
 
